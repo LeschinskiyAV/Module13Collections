ed0b25b3a152f7f6b2edfb0e124b673063b3d30d
Александр Лещинский <topjumbo@mail.ru>
2023-07-20 14:16:24 +1000

    правки форматирования строк для длины строки < 120Add project files.

---
+++ Task1362StopWatchTwo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace PhoneBook
+{
+    class Program
+    {
+        private static LinkedList<int> BenchList = new LinkedList<int>()
+        { };
+
+        static void Main(string[] args)
+        {
+            var watch = Stopwatch.StartNew();
+            BenchList.AddFirst(1);
+            Console.WriteLine($"Вставка LinkedList: {watch.Elapsed.TotalMilliseconds} мс");
+        }
+    }
+}
