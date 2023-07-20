e71a9a2184d9bed37993d60ebecdc84a04ed0fd5
Александр Лещинский <topjumbo@mail.ru>
2023-07-20 14:16:24 +1000

    Add project files.

---
+++ Task1361/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace PhoneBook
+{
+    class Program
+    {
+        private static List<int> BenchList = new List<int>()
+        { };
+
+        static void Main(string[] args)
+        {
+            var watch = Stopwatch.StartNew();
+            BenchList.Add(0);
+            Console.WriteLine($"Вставка List: {watch.Elapsed.TotalMilliseconds} мс");
+        }
+    }
+}
---
+++ Task1362/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.RegularExpressions;
+
+namespace WordCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\temp\data\Text1.txt";
+            if (File.Exists(filePath))
+            {
+                Dictionary<string, int> Words = new Dictionary<string, int>();
+                var text = File.ReadAllText(filePath);
+                var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+                var newString = string.Join(" ", Regex.Split(noPunctuationText, @"(?:\r\n|\n|\r)"));
+                string[] words = newString.Split(' ');
+                foreach (var word in words)
+                {
+                    if (word == "")
+                    {
+                        continue;
+                    }
+                    else if (Words.ContainsKey(word))
+                    {
+                        Words[word] += 1;
+                    }
+                    else
+                    {
+                        Words.Add(word, 1);
+                    }
+                }
+                var sortedWords = (from entry in Words orderby entry.Value descending select entry).Take(10).ToDictionary(pair => pair.Key, pair => pair.Value);
+                Console.WriteLine("top 10 words by usage, w/o punctuiation and return carrieage");
+                foreach (var SortedWord in sortedWords)
+                {
+                    Console.WriteLine($"Word: {SortedWord.Key}, Usages: {SortedWord.Value}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("I refuse to do work! Place file Text1.txt in C:\\temp\\data\\, please");
+            }
+        }
+    }
+}
